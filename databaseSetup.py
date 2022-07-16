ee7e00c326ad106a9f71ca9f8c7138d082de2322
Julianne Elbin <jelbin313@gmail.com>
2022-07-15 22:05:11 -0400

    moved the databse setup to its own sql file

--- databaseSetup.py
+++ databaseSetup.py
@@ -4,45 +4,17 @@ import sqlite3
 conn = sqlite3.connect('DichotomousKey.db')
 print("Successfully connected to database.")
 
-sql = """CREATE TABLE Keys (
-        KeyID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-        KeyName TEXT NOT NULL,
-        FirstNode INTEGER NOT NULL,
-        FOREIGN KEY (FirstNode) REFERENCES Nodes(NodeID)
-        );"""
-conn.execute(sql)
-print("Successfully created Keys table.")
+#open the sql file
+with open('databaseSetup.sql', 'r') as sql_file:
+    databaseSetup = sql_file.read()
 
-sql = """CREATE TABLE Nodes (
-            NodeID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-            PreviousNode INTEGER,
-            YesNode INTEGER,
-            NoNode INTEGER,
-            NodeType TEXT CHECK (NodeType IN ('Question', 'Species')),
-            NodeQuestion INTEGER,
-            NodeSpecies INTEGER,
-            FOREIGN KEY (NodeQuestion) REFERENCES Questions(QuestionID)
-            FOREIGN KEY (NodeSpecies) REFERENCES Species(SpeciesID)
-        );"""
-conn.execute(sql)
-print("Successfully created Nodes table.")
+#execute sql to create database
+conn.executescript(databaseSetup)
 
-sql = """CREATE TABLE Questions (
-            QuestionID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-            QuestionText Text NOT NULL
-        );"""
-conn.execute(sql)
-print("Successfully created Questions table.")
-
-sql = """CREATE TABLE Species (
-            SpeciesID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-            ScientificName TEXT NOT NULL,
-            CommonName TEXT,
-            Description TEXT,
-            Image TEXT
-        );"""
-conn.execute(sql)
-print("Successfully created Species table.")
-
+#commit changes to databse
 conn.commit()
+
+#close connection to the database
 conn.close()
+
+print("Successfully created database.")
